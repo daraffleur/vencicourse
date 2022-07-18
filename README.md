13075944058595c266321429467f48233410fd68
daraffleur <romaniuk.dana@gmail.com>
2022-07-18 11:21:15 +0000

    add telegram

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tc landing
+
+# TELEGRAM API
+1. Create Bot
+2. Add Bot to group
+3. curl  `https://api.telegram.org/bot<token>/getUpdates`
+4. Find Chat ID (message - chat["id"]) 
+4. Send msg to the group `curl  https://api.telegram.org/bot<token>/sendMessage -d text="A message from your bot" -d
+chat_id=<id>`
--- app/views/main.py
+++ app/views/main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 from flask import render_template, request, Blueprint, flash, redirect
 
@@ -9,23 +10,21 @@ from app.logger import log
 
 main_blueprint = Blueprint("main", __name__)
 EMAIL_ENDPOINT = os.environ.get("EMAIL_ENDPOINT")
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
-
-# def send_applicant_to_telegram_chat(text):
-#     method = "sendMessage"
-#     token = "2049371679:AAGLoC9AaUdNAaxrsgCYOEF15kiXZmRVTq8"
-#     chat_id = "-1001398146536"
-#     url = f"https://api.telegram.org/bot{token}/{method}"
-#     data = {"chat_id": chat_id, "text": text}
-#     requests.post(url, data=data)
-
+def send_applicant_to_telegram_chat(text):
+    method = "sendMessage"
+    token = TELEGRAM_TOKEN
+    chat_id = TELEGRAM_CHAT_ID
+    url = f"https://api.telegram.org/bot{token}/{method}"
+    data = {"chat_id": chat_id, "text": text}
+    requests.post(url, data=data)
 
 @main_blueprint.route("/", methods=["GET", "POST"])
 def index():
     form = ApplicantForm(request.form)
     if form.validate_on_submit():
-        # applicant = f"НОВА ЗАЯВКА НА КУРС\n\nПрізвище: {form.first_name.data}\nІм'я: {form.last_name.data}\n \nЕмейл: {form.email.data}\nНомер телефону: {form.phone.data}"
-        # send_applicant_to_telegram_chat(applicant)
         applicant = Applicant(
             first_name=form.first_name.data,
             last_name=form.last_name.data,
@@ -33,6 +32,8 @@ def index():
             phone=form.phone.data,
         )
         applicant.save()
+        applicant = f"NEW APPLICATION \n\nFirst Name: {form.first_name.data}\nLast Name: {form.last_name.data}\n \nEmail: {form.email.data}\nPhone Number: {form.phone.data}"
+        send_applicant_to_telegram_chat(applicant)
         flash(
             "Your application has been sent successfully! We will call you soon!",
             "success",
@@ -41,8 +42,7 @@ def index():
             log.DEBUG,
             "Your application has been sent successfully! We will call you soon!",
         )
-        # return redirect("https://tokarstudy.com.ua/#registration")
-        return redirect("http://localhost:5000/#registration")
+        return redirect("http://www.vencistankovcourse.info/#registration")
     elif form.is_submitted():
         for error in form.errors:
             for msg in form.errors[error]:
@@ -51,6 +51,5 @@ def index():
                     "Something went wrong... Please reload the page and try submitting an application again. ",
                     "danger",
                 )
-                # return redirect("https://tokarstudy.com.ua/#registration")
-                return redirect("http://localhost:5000/#registration")
+                return redirect("http://www.vencistankovcourse.info/#registration")
     return render_template("index.html", form=form)
